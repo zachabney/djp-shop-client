904ae92cd09e105193d4839607fd905827205981
Zach Abney <zach.abney192@gmail.com>
2019-05-23 15:30:54 -0500

    Change api endpoint

--- src/app/api.ts
+++ src/app/api.ts
@@ -3,5 +3,5 @@ import { isDevMode } from '@angular/core';
 export function getAPIBaseUrl(): string {
 	return isDevMode()
 		? 'http://localhost:3000'
-		: 'https://api.shop.djpcreative.com';
+		: 'https://api-shop.djpcreative.com';
 }
